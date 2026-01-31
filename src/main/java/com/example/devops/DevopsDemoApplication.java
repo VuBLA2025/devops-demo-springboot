9845a128f0c05899b7c9bc195fcddae593a87f00
bvu <bvu@paramountcommerce.com>
2026-01-31 21:12:37 +0700

    Add DevOps demo project files

---
+++ src/main/java/com/example/devops/DevopsDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void run(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+    
+    public static void main(String[] args) {
+        run(args);
+    }
+
+}
