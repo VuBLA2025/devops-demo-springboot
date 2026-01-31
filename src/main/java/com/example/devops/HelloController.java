8d3e4a0b417e9fe2657c5738b1f1a09ab3fa0e96
VuBLA2 <vubla2025@gmail.com>
2026-01-31 21:44:31 +0700

    Update HelloController

--- src/main/java/com/example/devops/HelloController.java
+++ src/main/java/com/example/devops/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, DevOps Learner! " + System.currentTimeMillis();
+        return "Hello, DevOps Learner! Test" + System.currentTimeMillis();
     }
 }
