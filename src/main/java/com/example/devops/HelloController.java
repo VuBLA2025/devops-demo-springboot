d826527a9654bafd12a1f750ab4e7e58808beaea
VuBLA2 <vubla2025@gmail.com>
2026-01-31 22:35:33 +0700

    test-error

--- src/main/java/com/example/devops/HelloController.java
+++ src/main/java/com/example/devops/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, DevOps Learner! Test" + System.currentTimeMillis();
+        return A"Hello, DevOps Learner! Test" + System.currentTimeMillis();
     }
 }
